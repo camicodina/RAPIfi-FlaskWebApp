42b367b059578f9eaa9ab4867c93ad1da11e5aff
Camila-Codina <Camila.Codina@ibm.com>
2020-10-27 00:55:15 -0300

    app.py

---
+++ app/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, redirect, url_for, request
+import csv
+
+def guardar_datos(nombre,email,wifi,telefono):
+    archivo = open('datos/datos_formulario.csv', 'a')
+    archivo.write("{},{},{},{}\n".format(nombre,email,wifi,telefono))
+    archivo.close()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/form', methods=["POST"])
+def form():
+    nombre = request.form.get("name")
+    email = request.form.get("email")
+    wifi = request.form.get("planes")
+    telefono = request.form.get("phone")
+
+    mensaje = "¡Se envió el formulario con éxito! Pronto lo contactaremos."
+    guardar_datos(nombre,email,wifi,telefono)
+    return render_template("index.html", mensaje=mensaje)
+
+@app.route('/datos')
+def datos():
+    with open('datos/datos_formulario.csv') as csv_file:
+        data = csv.reader(csv_file, delimiter=',')
+        clientes = []
+        for row in data:
+            clientes.append({
+            "nombre": row[0],
+            "email": row[1],
+            "plan": row[2],
+            "tel": row[3]
+            })
+    return render_template("datos.html", clientes=clientes)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+    
